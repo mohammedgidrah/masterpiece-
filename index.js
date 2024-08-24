b395582a26474073e85d76aad4ea098a8ca34907
mohammedgidrah <m7mgidrah@gmail.com>
2024-08-24 04:34:09 +0300

    updated

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 document.addEventListener('DOMContentLoaded', function() {
     const menuToggle = document.querySelector('.menu-toggle');
     const navLinks = document.querySelector('.links');
+    const bg = document.querySelector('header');
     const body = document.body;
 
     menuToggle.addEventListener('click', function() {
+        bg.classList.toggle('active');
         navLinks.classList.toggle('active');
         body.classList.toggle('show-sidebar');
     });
